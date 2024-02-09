629975d13d122505dbdfb843956f4ea37d12170b
avj416@login001.(none) <wbbeck1996@satx.rr.com>
2024-02-09 16:57:52 -0600

    Changed K_fib to meet the Homework criteria

--- assignment3/k_fib.sh
+++ assignment3/k_fib.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
+#
+# This program shows the K-Fibbonacci Sequence up to term N,
+# where N is the first input, and K is the second
+#
+#
 
 N=$1
-K=$2
+k=$2
 
-#######Force user to enter filenname.sh <N> <K> or return error message
-if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 <N> <K>"
+if [[ "$N" -le "0" ]]; then
+	echo "Please enter a positive, non-zero value for N"
+	exit 1
+elif [[ "$k" -le "0" ]]; then
+	echo "Please enter a positive, non-zero value for K; The Fibbonacci Sequence cannot have K < 1"
 	exit 1
 fi
 
-kfib(){
+echo "Going to term $N of the $k-Fibbonacci Sequence"
 
-local  N=$1
-local  K=$2
-
-#declare -a array  makes an indexed array
-#declare -i array  make the values integers
-local series
-declare -a series
-declare -i series
-
-if [ "$N" -le "$K" ]; then
-	echo "1"
+#establishing a limiter, in case N < K
+if [[ "$N" -le "$k" ]]; then
+	lmtr=$N
 else
-	series=( $(seq -s ' ' 1 $K) )
-
-	for ((i=K+1; i<=N; i++)); do
-		sum=0
-		for ((j=1;j<=K;j++)); do
-			sum=$((sum+series[i-j]))
-		done 
-		series[i]=$sum
-	done
-	echo "${series[N]}"
+	lmtr=$k
 fi
-}
 
-result=$(kfib $N $K)
+##initializing the initial 1-terms of the K-Fibbonacci Sequence
+#first making an array to append to
+fib=(1)
 
-echo "The $N-th term for a K=$K series is: $result"
+#appending ones until there are K ones in the array.
+for((i = 1; i<$lmtr; i+=1))
+do
+    fib+=(1)
+done
 
+#test for accuracy
+#echo ${fib[@]}
 
+#Finding the terms after the initial K ones.
+counter=0
+for((i = $k; i<$N; i++))
+do
+	#creating a variable toget the some of the previous K terms in the series
+	counter=0
+
+	#finding the previous k terms to the one being inserted, and adding them together
+	for((j = $i; j>($i-$k); j--))
+	do
+		#arrays have a zero-based index, whereas the code is going by 1. Correcting with n
+		n=j-1
+		counter=$(($counter+${fib[n]}))
+	done
+	fib+=($counter)
+done
+
+echo ${fib[@]}
--- assignment3/k_fibbo.sh
+++ assignment3/k_fibbo.sh
@@ -1,11 +1,58 @@
 #!/bin/bash
 #
-#This program shows the K-fibbonacci sequence up to term N,
-#where k is the first input, and N is the second
+# This program shows the K-Fibbonacci Sequence up to term N,
+# where N is the first input, and K is the second
 #
 #
 
-echo showing the k=$1-Fibbonacci sequence up to $2
+N=$1
+k=$2
 
+if [[ "$N" -le "0" ]]; then
+	echo "Please enter a positive, non-zero value for N"
+	exit 1
+elif [[ "$k" -le "0" ]]; then
+	echo "Please enter a positive, non-zero value for K; The Fibbonacci Sequence cannot have K < 1"
+	exit 1
+fi
 
+echo "Going to term $N of the $k-Fibbonacci Sequence"
 
+#establishing a limiter, in case N < K
+if [[ "$N" -le "$k" ]]; then
+	lmtr=$N
+else
+	lmtr=$k
+fi
+
+##initializing the initial 1-terms of the K-Fibbonacci Sequence
+#first making an array to append to
+fib=(1)
+
+#appending ones until there are K ones in the array.
+for((i = 1; i<$lmtr; i+=1))
+do
+    fib+=(1)
+done
+
+#test for accuracy
+#echo ${fib[@]}
+
+#Finding the terms after the initial K ones.
+counter=0
+for((i = $k; i<$N; i++))
+do
+	#creating a variable toget the some of the previous K terms in the series
+	counter=0
+
+	#finding the previous k terms to the one being inserted, and adding them together
+	for((j = $i; j>($i-$k); j--))
+	do
+		#arrays have a zero-based index, whereas the code is going by 1. Correcting with n
+		n=j-1
+		counter=$(($counter+${fib[n]}))
+	done
+	fib+=($counter)
+done
+
+echo ${fib[@]}
