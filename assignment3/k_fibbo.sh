f30a503f260cda8cfbe69c9aca0a2492bbd0a50e
avj416@login001.(none) <wbbeck1996@satx.rr.com>
2024-02-09 15:32:52 -0600

    Fixed issue with negative runtimes in outfiles. Replaced the run and out files with new ones made using the fixed code.

---
+++ assignment3/k_fibbo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#This program shows the K-fibbonacci sequence up to term N,
+#where k is the first input, and N is the second
+#
+#
+
+echo showing the k=$1-Fibbonacci sequence up to $2
+
+
+
