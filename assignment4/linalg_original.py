1315fa9d53335ff47ae2da4b5dfe6d80d345fb23
avj416@login001.(none) <wbbeck1996@satx.rr.com>
2024-02-13 18:26:51 -0600

    Saved the P2.1 linalg.py as linalg_original.py. Modified linalg.py for P2.2. Added job_linalg_study.slurm, along with a trial run's outputs.

--- assignment4/linalg_original.py
+++ assignment4/linalg_original.py
@@ -4,12 +4,11 @@ import sys
 
 stdout_stuff = sys.stdout
 
-N=10000
+N=5
 
 #print(N)
-stdout_stuff.write('\n\nN = '+str(N)+'\n')
+stdout_stuff.write(str(N)+'\n')
 
-#start time for making K and f
 tm_0=tm.time()
 
 #make an array to populate the  diagonal
@@ -26,34 +25,30 @@ K=(np.diag(DM,0)*2)+(np.diag(OD,-1)*-1)+(np.diag(OD,1)*-1)
 
 #Setting K at NN to be 1 (note: Numpy starts at a zero index)
 K[N-1,N-1]=1
-#print(K)
+print(K)
 
 #writing f
 f = np.zeros(N)
 f[N-1]=1/N
-#print(f)
+print(f)
 
-#end time for making k and f
 tm_f=tm.time()
 
 #computing the elapsed time
 tm_elapsed=tm_f-tm_0
-print('Time needed to make K and f: '+str(round(tm_elapsed,9)))
+print(round(tm_elapsed,9))
 
-#start time for solving the system
 ts_0=tm.time()
 
 #solving the system
 u=np.linalg.solve(K,f)
 
-#end time for solving the system
 ts_f = tm.time()
 
-#elapsed time for solving the system
 delta_ts='%.9f'%(ts_f-ts_0)
 stdout_stuff.write('Solver time elapsed: '+str(delta_ts)+'\n')
 
 print('u = ')
 print(u)
-print('The last term in u is: '+str(u[N-1])+'\n\n')
+print('The last term in u is: '+str(u[N-1]))
 
